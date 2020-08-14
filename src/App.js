04f9c015dc5452dfbb571f54e752ab93ad2cd2f3
Håvard Hjelmeseth <haavahje@stud.ntnu.no>
2020-08-14 21:55:48 +0200

    Added frameless titlebar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import React from "react";
+import TitleBar from "frameless-titlebar";
+
+const remote = window.require("electron").remote;
+const currentWindow = remote.getCurrentWindow();
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <header>
+        <TitleBar
+          currentWindow={currentWindow} // electron window instance
+          platform={process.platform} // win32, darwin, linux
+          onClose={() => currentWindow.close()}
+          onMinimize={() => currentWindow.minimize()}
+          onMaximize={() => {
+            if (!currentWindow.isMaximized()) {
+              currentWindow.maximize();
+            } else {
+              currentWindow.unmaximize();
+            }
+          }}
+          // when the titlebar is double clicked
+          onDoubleClick={() => {
+            if (!currentWindow.isMaximized()) {
+              currentWindow.maximize();
+            } else {
+              currentWindow.unmaximize();
+            }
+          }}
+        />
+      </header>
+    </div>
+  );
 }
 
 export default App;
